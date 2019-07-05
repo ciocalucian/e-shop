9dde4045eabb483c7d9d541554387daed325cd60
ciocalucian <cioca.lucian@gmail.com>
2019-07-06 00:03:46 +0300

    display components

--- e-shop/src/app/app-routing.module.ts
+++ e-shop/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { DetailsComponent } from './details/details.component';
+import { CartComponent } from './cart/cart.component';
+import { AdminComponent } from './admin/admin.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'main-page', component: MainPageComponent },
+  {path: 'details', component: DetailsComponent},
+  {path: 'cart', component: CartComponent },
+  {path: 'admin', component: AdminComponent },
+  {path: '', 
+    redirectTo: '/main-page',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
